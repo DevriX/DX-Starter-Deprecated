10ac8a0e039c6ded21bf7a47ea8d3f675fa4e98d
Stanimir-Georgiev <stanimir.georgiev_99@abv.bg>
2020-02-17 13:59:54 +0200

    add some comments

--- generateTheme.js
+++ generateTheme.js
@@ -3,23 +3,26 @@ const path = require('path');
 const replaceInFiles = require('replace-in-file');
 
 function generateTheme() {
+    //default values of the theme
     let fullName = "DevriX Starter";
     let shortName = "DX-Starter";
     let textDomain = "dxstarter";
     let packageName = "DevriX_Starter";
 
-
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-    let recursiveAsyncReadLine = function () {
+
+    let recursiveAsyncReadLine = function () { 
+        //textdomain field recursive validation
         rl.question('Please enter textDomain (dxstarter) ', function (textDomainInput) {
             const pattern = /^[a-z]+$/
             if (!textDomainInput.match(pattern)) {
                 console.log("Invalid textDomain! The textDomain should be only lower case letters")
                 recursiveAsyncReadLine();
             } else {
+                //user input
                 rl.question("Please enter theme fullName (DevriX Starter) ", function (fullNameInput) {
                     rl.question("Please enter theme shortName (DX-Starter) ", function (shortNameInput) {
                         rl.question("Please enter packageName (DevriX_Starter) ", function (packageNameInput) {
@@ -41,11 +44,12 @@ function generateTheme() {
                 });
             }
         });
-
     };
-    rl.on("close", function () {
-        const options = {
 
+    rl.on("close", function () {
+        //search and replace options
+        const options = {
+            //path to the folders
             files: [
                 path.resolve('./*'),
                 path.resolve('./assets/dist/css/*'),
@@ -54,7 +58,8 @@ function generateTheme() {
                 path.resolve('./template-parts/*'),
                 path.resolve('./templates/*'),
             ],
-
+            
+            //search expressions
             from: [/DevriX Starter/g, /DX-Starter/g, /dxstarter/g, /DevriX_Starter/g],
             to: [fullName, shortName, textDomain, packageName],
 
@@ -65,6 +70,8 @@ function generateTheme() {
             }
 
         };
+
+        //calling the replace function
         replaceInFiles(options).then(results => {
             console.log("Remember to add author name and description");
             console.log("Generated")
